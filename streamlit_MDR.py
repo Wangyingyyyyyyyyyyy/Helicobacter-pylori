7ce15be2fdc003754158a735d25d73eb57233b20
Wangyingyyyyyyyyyyy <143474615+Wangyingyyyyyyyyyyy@users.noreply.github.com>
2024-07-27 23:43:35 +0800

    Update streamlit_MDR.py

--- streamlit_MDR.py
+++ streamlit_MDR.py
@@ -8,8 +8,10 @@ import matplotlib.pyplot as plt
 if 'model' not in st.session_state:
     model = joblib.load('model.pkl')
     st.session_state["model"] = model
+    st.session_state["scaler"] = joblib.load('scaler.pkl')
 else:
     model = st.session_state["model"]
+    scaler = st.session_state["scaler"]
 
 st.set_page_config(layout="wide")
 
@@ -41,17 +43,19 @@ for i in range(3):
 with st.form("my_form"):
     col7, col8 = st.columns([5, 5])
     with col7:
-        a = st.selectbox("group_354", ("presence", "absence"))
-        b = st.selectbox("hyuA K524E", ("presence", "absence"))
-        c = st.selectbox("group_667", ("presence", "absence"))
-        d = st.selectbox("group_1303", ("presence", "absence"))
-        e = st.selectbox("pabC E150A", ("presence", "absence"))
+        a = st.selectbox("gyrA P116A", ("presence", "absence"))
+        b = st.selectbox("group_505", ("presence", "absence"))
+        c = st.selectbox("group_354", ("presence", "absence"))
+        d = st.selectbox("glmU E162T", ("presence", "absence"))
+        e = st.selectbox("HP0602 I67T", ("presence", "absence"))
+        f = st.selectbox("group_1303", ("presence", "absence"))
     with col8:
-        f = st.selectbox("omp18 K168R", ("presence", "absence"))
-        g = st.selectbox("HP0415 T247M", ("presence", "absence"))
-        h = st.selectbox("group_1307", ("presence", "absence"))
-        i = st.selectbox("group_541", ("presence", "absence"))
-        j = st.selectbox("omp13 G211A", ("presence", "absence"))
+        g = st.selectbox("group_541", ("presence", "absence"))
+        h = st.selectbox("omp13 L29A", ("presence", "absence"))
+        i = st.selectbox("ydjA", ("presence", "absence"))
+        j = st.selectbox("group_283", ("presence", "absence"))
+        k = st.selectbox("HP0757 G878A", ("presence", "absence"))
+        l = st.selectbox("HP0486 A11T", ("presence", "absence"))
     col4, col5, col6 = st.columns([2, 2, 6])
     with col4:
         submitted = st.form_submit_button("Predict")
@@ -62,15 +66,17 @@ with st.form("my_form"):
 
     if submitted:
 
-        X = pd.DataFrame([[a, b, c, d, e, f, g, h, i, j]],
-                         columns=['group_354', 'hyuA K524E', 'group_667', 'group_1303', 'pabC E150A', 'omp18 K168R', 'HP0415 T247M', 'group_1307', 'group_541', 'omp13 G211A'])
+        X = pd.DataFrame([[a, b, c, d, e, f, g, h, i, j, k, l]],
+                         columns=['gyrA P116A', 'group_505', 'group_354', 'glmU E162T', 'HP0602 I67T', 'group_1303',
+                                  'group_541', 'omp13 L29A', 'ydjA', 'group_283', 'HP0757 G878A', 'HP0486 A11T'])
     
         X = X.replace(["presence", "absence"], [1, 0])
     
-    
+        # 标准化用户输入数据
+        X_scaled = scaler.transform(X)
 
-        prediction = model.predict(X)[0]
-        Predict_proba = model.predict_proba(X)[:, 1][0]
+        prediction = model.predict(X_scaled)[0]
+        Predict_proba = model.predict_proba(X_scaled)[:, 1][0]
 
         if prediction == 0:
             st.subheader(f"The predicted result of Hp MDR:  Sensitive")
@@ -81,9 +87,9 @@ with st.form("my_form"):
 
         with st.spinner('force plot generation, please wait...'):
             explainer = shap.TreeExplainer(model)
-            shap_values = explainer.shap_values(X)
-            shap.force_plot(explainer.expected_value, shap_values[0], X.iloc[0].values, feature_names=['group_354', 'hyuA K524E', 'group_667', 'group_1303',
-                                                                                                       'pabC E150A', 'omp18 K168R', 'HP0415 T247M', 'group_1307', 'group_541', 'omp13 G211A'], matplotlib=True, show=False, figsize=(20, 5))
+            shap_values = explainer.shap_values(X_scaled)
+            shap.force_plot(explainer.expected_value, shap_values[0], X_scaled.iloc[0].values, feature_names=['gyrA P116A', 'group_505', 'group_354', 'glmU E162T', 'HP0602 I67T', 'group_1303',
+                                  'group_541', 'omp13 L29A', 'ydjA', 'group_283', 'HP0757 G878A', 'HP0486 A11T'], matplotlib=True, show=False, figsize=(20, 5))
             plt.xticks(fontproperties='Arial', size=16)
             plt.yticks(fontproperties='Arial', size=16)
             plt.tight_layout()
